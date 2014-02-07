b6b36b240566d0b66ca38858a532dd72f74c3dfc
Paul Hinze <paulh@instructure.com>
2014-02-07 15:28:30 -0600

    puppet provisioning: run apt-get update before pkg install
    when i booted a vagrant server i'd consistently get package install
    failures. running an apt-get update first flushes them out.
    
    also spruce up the puppet code so it passes lint.

--- puppet_provisioning/site.pp
+++ puppet_provisioning/site.pp
@@ -1,9 +1,11 @@
-node "precise" {
-  package {"ruby1.9.3": ensure => installed}
-  package {"git": ensure => installed}
+node 'precise' {
+  exec { '/usr/bin/apt-get update':
+  } ->
+  package {'ruby1.9.3': ensure => installed}
+  package {'git': ensure => installed}
 }
 
-node "centos6" {
-  package {"ruby": ensure => installed}
-  package {"git":  ensure => installed}
+node 'centos6' {
+  package {'ruby': ensure => installed}
+  package {'git':  ensure => installed}
 }
